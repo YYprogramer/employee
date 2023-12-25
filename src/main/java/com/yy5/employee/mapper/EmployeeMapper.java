c9f5574ea0a9a94b8b031468a49d929dcd864af2
YYprogramer <131517182+YYprogramer@users.noreply.github.com>
2023-12-26 05:12:06 +0900

    List型からOptional型に変更

--- src/main/java/com/yy5/employee/mapper/EmployeeMapper.java
+++ src/main/java/com/yy5/employee/mapper/EmployeeMapper.java
@@ -1,12 +1,14 @@
 package com.yy5.employee.mapper;
 
+import com.yy5.employee.entity.Employee;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
+import java.util.Optional;
 
 @Mapper
 public interface EmployeeMapper {
     @Select("select * from emplyee_list WHRER name LIKE CONCAT('%', #{name}, '%')")
-    List<Employee> findByName(String name);
+    Optional<Employee> findByName(String name);
 }
