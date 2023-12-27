9f2458f2ea0e1bf962c5a6533c3db8b965415dec
YYprogramer <131517182+YYprogramer@users.noreply.github.com>
2023-12-28 04:35:12 +0900

    全建検索が実行のためControllerを修正

--- src/main/java/com/yy5/employee/controller/EmployeeController.java
+++ src/main/java/com/yy5/employee/controller/EmployeeController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
 public class EmployeeController {
     public final EmployeeService employeeService;
@@ -13,6 +15,10 @@ public class EmployeeController {
         this.employeeService = employeeService;
     }
 
+    @GetMapping("/employees")
+    public Optional<Employee> getAll() {
+        return employeeService.findAll();
+    }
     @GetMapping("/employees/{employeeNumber}")
     public Employee getEmployee(@PathVariable("employeeNumber") int employeeNumber) {
         return employeeService.findEmployee(employeeNumber);
