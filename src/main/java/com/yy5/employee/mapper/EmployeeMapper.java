43d38eb538d3534d02e1d3469484a25c39f80109
YYprogramer <131517182+YYprogramer@users.noreply.github.com>
2024-01-04 05:16:38 +0900

    Mapperに@Insert/@Optionsアノテーションを作成

--- src/main/java/com/yy5/employee/mapper/EmployeeMapper.java
+++ src/main/java/com/yy5/employee/mapper/EmployeeMapper.java
@@ -1,7 +1,9 @@
 package com.yy5.employee.mapper;
 
 import com.yy5.employee.entity.Employee;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
@@ -14,4 +16,8 @@ public interface EmployeeMapper {
 
     @Select("select * from employees WHERE employeeNumber LIKE CONCAT('%', #{employeeNumber}, '%')")
     Optional<Employee> findByEmployee(int employeeNumber);
+
+    @Insert("INSERT INTO employees (name, age) VALUES (#{name}, #{age})")
+    @Options(useGeneratedKeys = true, keyProperty = "employeeNumber")
+    void insert(Employee employee);
 }
