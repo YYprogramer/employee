bec7f72aa1b217cdb1d7315be58dc55dc4fbb5c8
YYprogramer <131517182+YYprogramer@users.noreply.github.com>
2024-01-09 05:02:30 +0900

    Entityクラス内でEmployeeを２回定義していたので片方削除

--- src/main/java/com/yy5/employee/entity/Employee.java
+++ src/main/java/com/yy5/employee/entity/Employee.java
@@ -5,12 +5,6 @@ public class Employee {
     private String name;
     private int age;
 
-    public Employee(int employeeNumber, String name, int age) {
-        this.employeeNumber = employeeNumber;
-        this.name = name;
-        this.age = age;
-    }
-
     public Employee(String name, int age) {
         this.name = name;
         this.age = age;
