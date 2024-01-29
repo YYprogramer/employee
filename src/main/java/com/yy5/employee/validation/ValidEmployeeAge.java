75ccdeae581b6944b4890360906ec872d185bcf0
YYprogramer <131517182+YYprogramer@users.noreply.github.com>
2024-01-30 05:38:17 +0900

    ValidEmployeeAgeクラスのAgeカラムに対する＠Deprecatedおよび@NotBlankを削除

--- src/main/java/com/yy5/employee/validation/ValidEmployeeAge.java
+++ src/main/java/com/yy5/employee/validation/ValidEmployeeAge.java
@@ -3,7 +3,6 @@ package com.yy5.employee.validation;
 import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
 import jakarta.validation.ReportAsSingleViolation;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 import java.lang.annotation.ElementType;
@@ -11,14 +10,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Deprecated
 @NotNull
-@NotBlank
 @Constraint(validatedBy = {})
 @Target({ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
 @ReportAsSingleViolation
-
 public @interface ValidEmployeeAge {
     String message() default "無効な年齢です";
 
