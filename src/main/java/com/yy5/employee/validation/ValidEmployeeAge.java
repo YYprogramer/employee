f29b931afdb3a21d41d5c85b7ef2407373732098
YYprogramer <131517182+YYprogramer@users.noreply.github.com>
2024-01-31 04:24:43 +0900

    Ageカラムに@Max(65)を追加

--- src/main/java/com/yy5/employee/validation/ValidEmployeeAge.java
+++ src/main/java/com/yy5/employee/validation/ValidEmployeeAge.java
@@ -3,6 +3,7 @@ package com.yy5.employee.validation;
 import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
 import jakarta.validation.ReportAsSingleViolation;
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotNull;
 
 import java.lang.annotation.ElementType;
@@ -11,6 +12,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @NotNull
+@Max(65)
 @Constraint(validatedBy = {})
 @Target({ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
