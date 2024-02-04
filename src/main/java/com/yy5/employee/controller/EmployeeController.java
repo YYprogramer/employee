b1cb1181039671f0f85878dbcfbac9bd484decc9
YYprogramer <131517182+YYprogramer@users.noreply.github.com>
2024-02-05 04:18:59 +0900

    Controllerにおける不要なインポートの削除

--- src/main/java/com/yy5/employee/controller/EmployeeController.java
+++ src/main/java/com/yy5/employee/controller/EmployeeController.java
@@ -9,7 +9,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
