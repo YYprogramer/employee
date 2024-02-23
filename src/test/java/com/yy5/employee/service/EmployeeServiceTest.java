44c4ea224ce753665eef5f7de145da1a7e3a667b
YYprogramer <131517182+YYprogramer@users.noreply.github.com>
2024-02-24 05:33:08 +0900

    ServiceTestクラスの再作成

--- src/test/java/com/yy5/employee/service/EmployeeServiceTest.java
+++ src/test/java/com/yy5/employee/service/EmployeeServiceTest.java
@@ -1,7 +1,20 @@
 package com.yy5.employee.service;
 
+import com.yy5.employee.mapper.EmployeeMapper;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class EmployeeServiceTest {
+    @InjectMocks
+    EmployeeService employeeService;
+    @Mock
+    EmployeeMapper employeeMapper;
+
+    //Read機能のテスト
 
 }
