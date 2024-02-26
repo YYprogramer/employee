c837436d4f433690ec04158f5c8f7714048810ff
YYprogramer <131517182+YYprogramer@users.noreply.github.com>
2024-02-27 05:23:44 +0900

    MapperTestクラスの作成および全件取得テストの作成

--- src/test/java/com/yy5/employee/mapper/EmployeeMapperTest.java
+++ src/test/java/com/yy5/employee/mapper/EmployeeMapperTest.java
@@ -1,7 +1,39 @@
 package com.yy5.employee.mapper;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.github.database.rider.core.api.dataset.DataSet;
+import com.github.database.rider.spring.api.DBRider;
+import com.yy5.employee.entity.Employee;
+import org.junit.jupiter.api.Test;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DBRider
+@MybatisTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class EmployeeMapperTest {
+    @Autowired
+    EmployeeMapper employeeMapper;
+
+    // Readテスト 全件取得
+    @Test
+    @DataSet(value = "datasets/employees.yml")
+    @Transactional
+    void 全ての社員情報が取得できること() {
+        List<Employee> employees = employeeMapper.findAll();
+        assertThat(employees)
+                .hasSize(3)
+                .contains(
+                        new Employee(1,"テスト1",21),
+                        new Employee(2,"テスト2",22),
+                        new Employee(3,"テスト3",23)
+                );
+    }
 
 }
