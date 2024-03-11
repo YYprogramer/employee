2ea3cd2e9edc496d0bf41f6d63bbcf79601dd989
YYprogramer <131517182+YYprogramer@users.noreply.github.com>
2024-03-11 19:57:19 +0900

    正常系テストのコメントを削除およびバリデーションテストの実装

--- src/test/java/com/yy5/employee/controller/EmployeeControllerTest.java
+++ src/test/java/com/yy5/employee/controller/EmployeeControllerTest.java
@@ -4,6 +4,8 @@ import com.yy5.employee.controller.request.EmployeeRequest;
 import com.yy5.employee.controller.response.EmployeeResponse;
 import com.yy5.employee.entity.Employee;
 import com.yy5.employee.service.EmployeeService;
+import jakarta.validation.Constraint;
+import jakarta.validation.ConstraintViolationException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -27,22 +29,20 @@ class EmployeeControllerTest {
 
     @Test
     void クリエイトリクエストを受け取ったとき社員情報を登録すること() {
-        // モックの準備
         when(employeeService.insert(anyString(), anyInt())).thenReturn(new Employee("iwatsuki", 29));
-
-        // テスト実行
         EmployeeRequest request = new EmployeeRequest("iwatsuki", 29);
         ResponseEntity<EmployeeResponse> responseEntity = employeeController.insert(request, UriComponentsBuilder.newInstance());
-
-        // モックの検証
         verify(employeeService, times(1)).insert(eq("iwatsuki"), eq(29));
-
-        // レスポンスの検証
         assertNotNull(responseEntity);
         assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
         assertNotNull(responseEntity.getBody());
         assertEquals("employee created", responseEntity.getBody().getMessage());
     }
 
-
+    @Test
+    void クリエイトリクエストを受けっとたとき名前及び年齢情報がnullだとバリデーションが実行される() {
+        EmployeeRequest request = new EmployeeRequest(null,null);
+        assertThrows(ConstraintViolationException.class, () -> employeeController.insert(request, UriComponentsBuilder.newInstance()));
+        verify(employeeService, never()).insert(anyString(), anyInt());
+    }
 }
