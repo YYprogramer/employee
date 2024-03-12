27f3d745cea600ccbf5286e4dcf9c68ee3cf93b0
YYprogramer <131517182+YYprogramer@users.noreply.github.com>
2024-03-13 04:33:54 +0900

    テストが成功するように@SpringBootTestを削除

--- src/test/java/com/yy5/employee/controller/EmployeeControllerTest.java
+++ src/test/java/com/yy5/employee/controller/EmployeeControllerTest.java
@@ -29,7 +29,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(MockitoExtension.class)
-@SpringBootTest
 class EmployeeControllerTest {
     @InjectMocks
     private EmployeeController employeeController;
