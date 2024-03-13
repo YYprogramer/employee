c944a87770c513228e21f1175bac99d65e9ad85e
YYprogramer <131517182+YYprogramer@users.noreply.github.com>
2024-03-14 05:41:10 +0900

    結合テスト　社員情報を全件取得する

--- src/test/java/integrationtest/EmployeeRestApiIntegrationTest.java
+++ src/test/java/integrationtest/EmployeeRestApiIntegrationTest.java
@@ -1,4 +1,33 @@
 package integrationtest;
 
+import com.github.database.rider.core.api.dataset.DataSet;
+import com.github.database.rider.spring.api.DBRider;
+import com.yy5.employee.EmployeeApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest(classes = EmployeeApplication.class)
+@AutoConfigureMockMvc
+@DBRider
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class EmployeeRestApiIntegrationTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    @DataSet(value = "datasets/employees.yml")
+    @Transactional
+    void 社員情報が全件取得できること() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/employees"))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+
+    }
 }
