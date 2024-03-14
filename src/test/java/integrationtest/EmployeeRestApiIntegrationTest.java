97f7642d33c11b5c4459d223e5a4ecceb68f3830
YYprogramer <131517182+YYprogramer@users.noreply.github.com>
2024-03-14 19:56:17 +0900

    全社員情報を取得する結合テストの実装

--- src/test/java/integrationtest/EmployeeRestApiIntegrationTest.java
+++ src/test/java/integrationtest/EmployeeRestApiIntegrationTest.java
@@ -4,6 +4,8 @@ import com.github.database.rider.core.api.dataset.DataSet;
 import com.github.database.rider.spring.api.DBRider;
 import com.yy5.employee.EmployeeApplication;
 import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -12,6 +14,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.nio.charset.StandardCharsets;
 
 @SpringBootTest(classes = EmployeeApplication.class)
 @AutoConfigureMockMvc
@@ -25,9 +29,17 @@ public class EmployeeRestApiIntegrationTest {
     @Test
     @DataSet(value = "datasets/employees.yml")
     @Transactional
-    void 社員情報が全件取得できること() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/employees"))
-                .andExpect(MockMvcResultMatchers.status().isOk());
-
+    void getallがリクエストされた時社員情報が全件取得できること() throws Exception {
+        String response = mockMvc.perform(MockMvcRequestBuilders.get("/employees"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
+        JSONAssert.assertEquals("""
+                         [
+                         {"employeeNumber":1,"name":"スティーブ","age":21},
+                         {"employeeNumber":2,"name":"マーク","age":20},
+                         {"employeeNumber":3,"name":"ジェフ","age":30}
+                         ]
+                        """
+                , response, JSONCompareMode.STRICT);
     }
 }
